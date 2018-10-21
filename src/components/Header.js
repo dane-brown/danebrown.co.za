5ca3d4864c0306f891d291fe75779a6470c99d05
dane-brown <dane@propdata.net>
2018-10-21 10:38:42 +0200

    Added to header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,26 @@ class Header extends Component {
   render() {
     return (
       <section className="header">
-        <div className="headerLogo">Dane Brown</div>
+        <div className="headerContent">
+          <a href="/" className="headerLogo">Dane Brown</a>
+          <ul className="headerNavigation">
+            <a href="#">
+              <li>Profile</li>
+            </a>
+            <a href="#">
+              <li>Experience</li>
+            </a>
+            <a href="#">
+              <li>Skills</li>
+            </a>
+            <a href="#">
+              <li>Projects</li>
+            </a>
+            <a href="#">
+              <li>Contact</li>
+            </a>
+          </ul>
+        </div>
         <Typist className="headerHello" avgTypingDelay={300} delay={150} cursor={{show:false,}}>
           {words}
           <Typist.Backspace count={1} delay={600} />
