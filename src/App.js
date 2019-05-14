b1e29758beccc4acf8ddaf0efb1f0a9aaca7073a
Dane Brown <dane@lottostar.co.za>
2019-05-14 11:25:53 +0200

    Trying to improve email send

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Panel from './Panel';
-import ToDo from './components/ToDo';
-import NoMatch from './NoMatch';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import emailtest from "./views/emailtest";
+import NoMatch from "./NoMatch";
 
 // Styles
-import './css/App.css';
+import "./css/App.css";
 
 class App extends Component {
-
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/panel/" component={Panel}></Route>
-          <Route exact path="/panel/todo/" component={ToDo}></Route>
-          <Route component={NoMatch}></Route>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/email" component={emailtest} />
+          <Route component={NoMatch} />
         </Switch>
       </Router>
     );
